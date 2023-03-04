e7e3836972ffbae24fb593c7cc62072416ac5789
Nathan Tran <nathantran044@utexas.edu>
2023-03-03 23:26:00 -0600

    nathan changes to University

--- University.js
+++ University.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     Dimensions,
     SafeAreaView,
@@ -6,31 +6,125 @@ import {
     StyleSheet,
     Text,
     TouchableOpacity,
+    View,
 } from 'react-native';
 
 export default function University({ navigation }) {
 
+    const [currentDate, setcurrentDate] = useState('')
+
+    const monthName = ['January', 'Febuary', 'March', 'April', 'May', 
+        'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+    useEffect(() => {
+        var date = new Date().getDate()
+        var month = new Date().getMonth()
+
+        setcurrentDate(
+            monthName[month] + ' ' + date
+        )
+    }, [])
+
     return (
-        <SafeAreaView style={styles.mainView}>
             <ScrollView>
-                <Text style={styles.title}>University Screen</Text>
-                <Text style={styles.title}>University Screen</Text>
-                <TouchableOpacity
+                <SafeAreaView style={styles.mainView}>
+                <Text style={styles.dateStyle}>
+                    {currentDate}
+                </Text>
+                </SafeAreaView>
+                <View
                     style={{
-                        margin: 20,
-                        backgroundColor: '#65b86c',
-                        borderRadius: 20,
-                        padding: 10,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}
-                    onPress={() => navigation.navigate('Login')}>
-                    <Text style={{ color: 'black', fontWeight: '600', fontSize: 18 }}>
-                        Go to Login Page
-                    </Text>
-                </TouchableOpacity>
+                        paddingTop: 0,
+                        padding: 0,
+                        backgroundColor: 'white',
+                        position: 'absolute',
+                        top: 170,
+                        bottom: 0,
+                        width: 390,
+                        borderRadius: 40
+                      }}>
+                    <TouchableOpacity
+                        style={{
+                            margin: 25,
+                            marginTop: 50,
+                            backgroundColor: '#fa9a50',
+                            borderRadius: 50,
+                            padding: 35,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            shadowColor: 'rgba(0, 0, 0, 0.8)',
+                            shadowOpacity: 0.3,
+                            elevation: 20,
+                            shadowRadius: 5 ,
+                            shadowOffset : { width: 1, height: 10},
+                        }}
+                        onPress={() => navigation.navigate('Login')}>
+                        <Text style={{ color: 'black', fontWeight: '600', fontSize: 22 }}>
+                            Breakfast
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{
+                            margin: 25,
+                            marginTop: 10,
+                            backgroundColor: '#fa9a50',
+                            borderRadius: 50,
+                            padding: 35,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            shadowColor: 'rgba(0, 0, 0, 0.8)',
+                            shadowOpacity: 0.3,
+                            elevation: 20,
+                            shadowRadius: 5 ,
+                            shadowOffset : { width: 1, height: 10},
+                        }}
+                        onPress={() => navigation.navigate('Login')}>
+                        <Text style={{ color: 'black', fontWeight: '600', fontSize: 22 }}>
+                            Lunch
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{
+                            margin: 25,
+                            marginTop: 10,
+                            backgroundColor: '#fa9a50',
+                            borderRadius: 50,
+                            padding: 35,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            shadowColor: 'rgba(0, 0, 0, 0.8)',
+                            shadowOpacity: 0.3,
+                            elevation: 20,
+                            shadowRadius: 5 ,
+                            shadowOffset : { width: 1, height: 10},
+                        }}
+                        onPress={() => navigation.navigate('Login')}>
+                        <Text style={{ color: 'black', fontWeight: '600', fontSize: 22 }}>
+                            Dinner
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{
+                            margin: 25,
+                            marginTop: 50,
+                            backgroundColor: '#d75c42',
+                            borderRadius: 10,
+                            padding: 50,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            shadowColor: 'rgba(0, 0, 0, 0.8)',
+                            shadowOpacity: 0.3,
+                            elevation: 20,
+                            shadowRadius: 5 ,
+                            shadowOffset : { width: 1, height: 10},
+                        }}
+                        onPress={() => navigation.navigate('Login')}>
+                        <Text style={{ color: 'black', fontWeight: '700', fontSize: 30 }}>
+                            Requests
+                        </Text>
+                    </TouchableOpacity>
+                </View>
             </ScrollView>
-        </SafeAreaView>
     );
 }
 
@@ -40,11 +134,16 @@ const styles = StyleSheet.create({
         width: Dimensions.get('window').width,
         backgroundColor: '#121212',
     },
-    title: {
-        fontSize: 20,
-        fontWeight: '800',
+    // title: {
+    //     fontSize: 20,
+    //     fontWeight: '800',
+    //     textAlign: 'center',
+    //     marginTop: 10,
+    //     color: 'white',
+    // },
+    dateStyle: {
         textAlign: 'center',
-        marginTop: 10,
-        color: 'white',
-    },
+        fontSize: 45,
+        color: 'white'
+    }
 });
