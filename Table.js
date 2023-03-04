b1da51baf4d233d9f6573d48d0e0759f669e2907
anooprac <anooprac@utexas.edu>
2023-03-04 10:47:11 -0600

    branch

---
+++ Table.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Table, TableWrapper, Row, Rows } from 'react-native-table-component';
+import RNCSV from 'react-native-csv-parser';
+
+RNCSV.parse()
